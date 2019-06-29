4d20dc47d31b4d376273d50f7d538318c869aaee
Carlin Liao <carlin.liao@berkeley.edu>
2019-06-28 19:00:36 -0500

    changed my mind about node selection method

--- central_node_selector.py
+++ central_node_selector.py
@@ -4,10 +4,9 @@ import numpy as np
 
 def central_node_selector(nodeFilename, proportion=.25, make_plot=True):
     '''
-    Select the 
+    Select some provided proportion of nodes closest to the center and return 
+    both a set of node IDs and, optionally, a plot of the selected nodes.
     '''
-    if proportion >= .5 or proportion <= 0:
-        raise ValueError('Proportion must be between 0 and .5 exclusive.')
 
     df = pd.read_csv(nodeFilename, delim_whitespace=True, index_col=0, usecols=['Node', 'X', 'Y'])
 
@@ -17,21 +16,18 @@ def central_node_selector(nodeFilename, proportion=.25, make_plot=True):
     ymin = df['Y'].min()
     ymax = df['Y'].max()
 
-    # define coords of bounding box to return
-    excludefactor = .5-proportion/2
-    xexcludelen = excludefactor*(xmax-xmin)
-    xboxmin = xmin + xexcludelen
-    xboxmax = xmax - xexcludelen
-    yexcludelen = excludefactor*(ymax-ymin)
-    yboxmin = ymin + yexcludelen
-    yboxmax = ymax - yexcludelen
-
-    # select for nodes
-    nodes = set(df[(df['X'] > xboxmin) & (df['X'] < xboxmax) & (df['Y'] > yboxmin) & (df['Y'] < yboxmax)].index.to_list())
+    # find distance of each node to center and select for given proportion of closest nodes
+    xdel = xmax-xmin
+    ydel = ymax-ymin
+    xc = xmin + xdel/2
+    yc = ymin + ydel/2
+    df['distsq'] = (df['X']-xc)**2 + (df['Y']-yc)**2
+    df.sort_values('distsq', inplace=True)
+    nodes = set(df.index[0:int(df.shape[0]*proportion)].to_list())
 
     # plot to verify
     if make_plot:
-        fig = plt.figure(figsize=(10,10*(yexcludelen/xexcludelen)))
+        fig = plt.figure(figsize=(10,10*(ydel/xdel)))
         selected_nodes = df.index.isin(nodes)
         plt.scatter(df.loc[~selected_nodes,'X'], df.loc[~selected_nodes,'Y'], c='k')
         plt.scatter(df.loc[selected_nodes,'X'], df.loc[selected_nodes, 'Y'], c='r')
