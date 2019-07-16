7cd8f544e084e0cc9d9834ade04dae68ca607040
Carlin Liao <carlin.liao@berkeley.edu>
2019-07-16 18:02:49 -0500

    added more output metrics

--- py/output_reader.py
+++ py/output_reader.py
@@ -5,14 +5,14 @@ import pandas as pd
 
 from py.od_perturber import od_perturber
 
-def capacity_reader(networkFileName):
+def net_reader(networkFileName):
     """
     Pulls link capacity data from the TNTP data format.  In keeping with
     this format, the zones/centroids are assumed to have the lowest node
     IDs (1, 2, ..., numZones).
     """
     try:
-        capacities = pd.Series()
+        df = pd.DataFrame(columns=['capacity', 'length'])
 
         with open(networkFileName, "r") as networkFile:
             fileLines = networkFile.read().splitlines()
@@ -36,20 +36,22 @@ def capacity_reader(networkFileName):
                     
                 # Create link                                
                 linkID = '(' + str(data[0]).strip() + "," + str(data[1]).strip() + ')'
-                capacities[linkID] = float(data[2])
+                df.loc[linkID] = [float(data[2]), float(data[3])]
         
-        return capacities
+        return df
         
     except IOError:
         print("\nError reading network file %s" % networkFile)
         traceback.print_exc(file=sys.stdout) 
 
-def output_reader(outputFilename, networkFileName=None, numZones=0):
+def output_reader(outputFilename, networkFileName=None, numZones=0, true_flows=None, focus_link=None):
     '''
     Process output from `tap-b` and returns resulting DataFrame and total system travel time TSTT
 
     numZones:   The number of zones in the network, used to filter out centroid connectors.
                 Ignored if value passed is None, 0, or the same number as there are nodes.
+    true_flows: pandas Series of flows with link IDs matching those of the input network.
+    focus_link: Link ID to return V/C value for. (Base case's most congested link suggested.)
     '''
     df = pd.read_csv(outputFilename, sep=' ', index_col=1, skiprows=2, header=None, names=[
         'ID',
@@ -64,15 +66,25 @@ def output_reader(outputFilename, networkFileName=None, numZones=0):
     numNodes = pd.DataFrame([heads, tails]).values.max()
 
     if networkFileName:
-        df['capacity'] = capacity_reader(networkFileName)
+        df[['capacity', 'length']] = net_reader(networkFileName)
         
     if numZones < numNodes: # there are centroid connectors, so remove those
         df.drop(df.index[((heads <= numZones) | (tails <= numZones))], 0, inplace=True)
     
     tstt = (df['cost']*df['flow']).sum()
+    toReturn = [df, tstt]
 
     if networkFileName:
         weighted_vc = (df['flow']**2 / (df['capacity'] * df['flow'].sum())).mean()
-        return df, tstt, weighted_vc
-    else:
-        return df, tstt
+        vmt = (df['flow']*df['length']).sum()
+        toReturn += [weighted_vc, vmt]
+    
+    if true_flows is not None:
+        close_flow_link_pc = ((df['flow']/true_flows - 1).abs() <= 0.01).sum()/df.shape[0]
+        toReturn.append(close_flow_link_pc)
+    
+    if focus_link:
+        focus_link_vc = df.loc[focus_link, 'flow']/df.loc[focus_link, 'capacity']
+        toReturn.append(focus_link_vc)
+
+    return tuple(toReturn)
